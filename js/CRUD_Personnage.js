94cefa15042b1669a31d1b9a6c3678ef24e1216e
TevaPhilippe05 <philippe.teva.pro@gmail.com>
2025-03-20 15:01:37 +0100

    Personnages update

--- js/CRUD_Personnage.js
+++ js/CRUD_Personnage.js
@@ -2,12 +2,12 @@ const connexion = require("./db_connect");
 const mysql = require('mysql');
 
 function insert_perso(nom, prenom, email, mdp){
-    var sql = "INSERT INTO Personnage (nom, prenom, email, mdp) VALUES ('"+nom+"', '"+prenom+"', '"+email+"', '"+mdp+"')";
+    var sql = "INSERT INTO Personnages (nom, prenom, email, mdp) VALUES ('"+nom+"', '"+prenom+"', '"+email+"', '"+mdp+"')";
     connexion.conn.query(sql);
 }
 
 function select_perso(id){
-    var sql = "SELECT nom, prenom, email FROM Personnage WHERE id='" + id + "'";
+    var sql = "SELECT nom, prenom, email FROM Personnages WHERE id='" + id + "'";
     return new Promise((resolve, reject) => {
         connexion.conn.query(sql, (err, result) => {
             if (err) {
@@ -20,7 +20,7 @@ function select_perso(id){
 }
 
 function select_perso_connexion(email, mdp){
-    var sql = "SELECT id FROM Personnage WHERE email='" + email + "' AND mdp='" + mdp + "'";
+    var sql = "SELECT id FROM Personnages WHERE email='" + email + "' AND mdp='" + mdp + "'";
     return new Promise((resolve, reject) => {
         connexion.conn.query(sql, (err, result) => {
             if (err) {
--- sql/init-base.sql
+++ sql/init-base.sql
@@ -2,7 +2,7 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-CREATE TABLE `Personnage` (
+CREATE TABLE `Personnages` (
   `id` int(11) NOT NULL,
   `nom` varchar(30) NOT NULL,
   `prenom` varchar(30) NOT NULL,
@@ -10,9 +10,9 @@ CREATE TABLE `Personnage` (
   `mdp` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-ALTER TABLE `Personnage`
+ALTER TABLE `Personnages`
   ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `Personnage`
+ALTER TABLE `Personnages`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
