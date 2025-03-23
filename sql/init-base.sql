e290d02581fac41b971726e36f3761efd1465309
TevaPhilippe05 <philippe.teva.pro@gmail.com>
2025-03-23 16:57:38 +0100

    Update init-base.sql

--- sql/init-base.sql
+++ sql/init-base.sql
@@ -2,7 +2,7 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-CREATE TABLE `Personnages` (
+CREATE TABLE `Personnage` (
   `id` int(11) NOT NULL,
   `nom` varchar(30) NOT NULL,
   `prenom` varchar(30) NOT NULL,
@@ -10,9 +10,9 @@ CREATE TABLE `Personnages` (
   `mdp` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-ALTER TABLE `Personnages`
+ALTER TABLE `Personnage`
   ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `Personnages`
+ALTER TABLE `Personnage`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
