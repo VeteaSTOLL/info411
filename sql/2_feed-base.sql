e30c63b862367f77862f1434d5b4bdc159293447
TevaPhilippe05 <philippe.teva.pro@gmail.com>
2025-03-26 22:17:43 +0100

    ordre fichier sql

--- sql/2_feed-base.sql
+++ sql/2_feed-base.sql
@@ -1,3 +1,5 @@
-INSERT INTO `personnage` (`id`, `nom`, `prenom`, `email`, `mdp`) VALUES
+use info411;
+
+INSERT INTO `Personnage` (`id`, `nom`, `prenom`, `email`, `mdp`) VALUES
 (1, 'stoll', 'vetea', 'vetea.stoll@gmail.com', 'vtcool'),
 (2, 'philippe', 'teva', 'teva.philippe@gmail.com', 'tevcool');
