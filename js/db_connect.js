649a642e343063aa16edc1b84532e099ac2a255a
TevaPhilippe05 <philippe.teva.pro@gmail.com>
2025-03-30 14:51:22 +0200

    modif du fichier de connection a la db

--- js/db_connect.js
+++ js/db_connect.js
@@ -2,9 +2,9 @@ const mariadb = require('mariadb');
 
 const pool = mariadb.createPool({
   host: 'localhost',
-  port: 3306,
-  user: 'root', 
-  password: '', 
+  port: 3307,
+  user: 'vttvcool', 
+  password: 'tvvtcool', 
   database: 'info411',
   connectionLimit: 10
 });
