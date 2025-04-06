ea0078518b9bce7d158b665ea50f4bc043d915ab
Vetea STOLL <vetea.stoll@gmail.com>
2025-04-06 22:18:27 +0200

    var env 3

--- sql/2_feed-base.sql
+++ sql/2_feed-base.sql
@@ -1,5 +1,6 @@
 use info411;
 
 INSERT INTO `Personnage` (`id`, `nom`, `prenom`, `email`, `mdp`, `popularite`) VALUES
-(1, 'stoll', 'vetea', 'vetea.stoll@gmail.com', 'vtcool', 100),
-(2, 'philippe', 'teva', 'teva.philippe@gmail.com', 'tevcool', 100);
+(1, 'stoll', 'vetea', 'vetea.stoll@gmail.com', 'vtcool', 800),
+(2, 'philippe', 'teva', 'teva.philippe@gmail.com', 'tevcool', 300),
+(3, 'cuneo', 'noah', 'noah.cuneo@gmail.com', 'noahcool', 500);
