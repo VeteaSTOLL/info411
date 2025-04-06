72e761ff00bb153a4825752d1358dbd2d266f8c5
Vetea STOLL <vetea.stoll@gmail.com>
2025-04-06 22:02:00 +0200

    var env 2

--- sql/2_feed-base.sql
+++ sql/2_feed-base.sql
@@ -1,4 +1,4 @@
-use ${TABLE_DB};
+use info411;
 
 INSERT INTO `Personnage` (`id`, `nom`, `prenom`, `email`, `mdp`, `popularite`) VALUES
 (1, 'stoll', 'vetea', 'vetea.stoll@gmail.com', 'vtcool', 100),
